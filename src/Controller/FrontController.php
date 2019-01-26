53b3210fd00b10ca8c55a5bf9c5a112e3e5ebd06
steph28 <stephane.soucheyre@gmail.com>
2019-01-26 18:31:59 +0100

    Ajout vues

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -2,24 +2,116 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
-
+use App\Entity\Post;
 use App\Repository\PostRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class FrontController extends AbstractController
 {
     /**
-     * @Route("/", name="front")
+     * @Route("/", name="accueil")
      */
     public function indexAction(PostRepository $repository)
     {
         $posts = $repository->findAll();
+        $categories = $repository->findAll();
+
         $params = array(
+            'title' => 'accueil',
             'posts' => $posts,
-            'title' => 'Accueil',
+            'categories' => $categories,
         );
 
-        return $this->render('front/accueil.html.twig', $params);
+        return $this->render('front/index.html.twig', $params);
     }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function loginAction()
+    {
+        return $this->render('front/login.html.twig', [
+            'title' => 'login',
+        ]);
+    }
+
+    /**
+     * @Route("/inscription/", name="registration")
+     */
+    public function registrationAction()
+    {
+        $params = array(
+            'title' => 'inscription',
+        );
+
+        return $this->render('front/inscription.html.twig', $params);
+    }
+
+    /**
+     * @Route("/video/{id}", name="video")
+     */
+    public function videoAction(Post $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $video = $em->find(Post::class, $id);
+
+        $params = array(
+            'title' => 'accueil',
+            'video' => $video,
+        );
+
+        return $this->render('front/video.html.twig', $params);
+    }
+
+    /**
+     * @Route("/initiation/", name="initiation")
+     */
+    public function initiationAction()
+    {
+        // $posts = $repository->findAll();
+
+        // $params = array(
+        //     'title' => 'initiation',
+        //     'posts' => $posts
+        // );
+
+        // return $this->render('front/initiation.html.twig', $params);
+        return $this->render('front/initiation.html.twig');
+    }
+
+    /**
+     * @Route("/perfectionnement/", name="improvement")
+     */
+    public function improvementAction()
+    {
+        // $posts = $repository->findAll();
+
+        // $params = array(
+        //     'title' => 'perfectionnement',
+        //     'posts' => $posts
+        // );
+
+        // return $this->render('front/improvement.html.twig', $params);
+        return $this->render('front/improvement.html.twig');
+    }
+
+    // /**
+    //  * @Route("/resultats/", name="search_results")
+    //  * @Template("@learn-python/App/templates/base.html.twig")
+    //  */
+    // public function searchAction(Request $request)
+    // {
+    //     $em = $this->getDoctrine()->getManager();
+ 
+    //     $form = $request->query->all('form');
+    //     $search = $form['search'];
+        
+    //     $resultats = $em->getRepository(Post::class)->find($search);
+    //     // Ici on utilise une requête créée dans le GeneralRepository
+ 
+    //     return ['search' => $search, 'resultats' => $resultats];
+    // }
 }
--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,18 +2,89 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Repository\PostRepository;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\PostType;
+use Knp\Component\Pager\PaginatorInterface;
 
 class PostController extends AbstractController
 {
     /**
-     * @Route("/post", name="post")
+     * @Route("/post", name="post_show")
      */
-    public function index()
+    public function indexAction(PostRepository $repository, PaginatorInterface $paginator, Request $request)
     {
-        return $this->render('post/index.html.twig', [
-            'controller_name' => 'PostController',
+        $query = $repository->findAll(PostType::class);
+
+        $pagination = $paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+            5
+        );
+
+        $params = array(
+            'title' => 'initiation',
+            'pagination' => $pagination
+        );
+
+        return $this->render('post/post_show.html.twig', $params);
+    }
+
+    /**
+     * @Route("/post/new", name="post_create")
+     * @Route("/post/{id}/edit", name="post_edit")
+     */
+    public function formAction(Post $post = null, Request $request, ObjectManager $manager)
+    {
+        if (!$post) {
+           $post = new Post();
+        }        
+
+        $post->setTitle("Titre d'exemple")
+             ->setContent("Le contenu de l'article");
+
+        $form = $this->createForm(PostType::class, $post);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if (!$post->getId()) {
+                $post->setDate(new \DateTime());
+            }
+
+            $manager->persist($post);
+            $manager->flush();
+
+            return $this->redirectToRoute('video', ['id' => $post->getId()]);
+        }
+
+        return $this->render('post/create.html.twig', [
+            'formPost' => $form->createView(),
+            'editMode' => $post->getId() !== null
         ]);
+    }
+
+    /**
+     * @Route("/post/delete/{id}/", name="post_delete")
+     * Page qui supprime un post
+     */
+    public function postDeleteAction($id, Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $post = $em->find(Post::class, $id);
+
+        $em->remove($post);
+        $em->flush();
+
+        $session = $request->getSession();
+        $session->getFlashBag()->add("success", 'La commande ' . $id . ' a bien été supprimé!');
+
+        return $this->redirectToRoute('post_show');
     }
 }
